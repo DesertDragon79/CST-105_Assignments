043d500323ca88f2501fc8ce139df0e2f1812464
DESKTOP-9T0H1HC\DesertDragon79 <michael.kibler@rocketmail.com>
2017-06-30 18:02:28 -0700

    Week 1 redo

---
+++ MyName/src/myname/MyName.java
@@ -0,0 +1,17 @@
+/*
+ Created by Michael
+Last edit 6-29-2017
+ */
+package myname;
+
+
+public class MyName {
+
+    
+    public static void main(String[] args) {
+        System.out.println("My name is Michael Kibler");
+        System.out.println("My favorite color is red");
+        System.out.println("If I won the the lottery, I would save it for when I need it.");        
+    }
+    
+}
